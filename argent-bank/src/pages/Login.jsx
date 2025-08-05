dd0c50012db11eefded0b1513c724c70123139f4
TAREK <nasantarek6@gmail.com>
2025-08-05 14:32:49 +0200

    l’application React créée. les différentes routes gérées par React Router. les pages HTML/CSS de l’application intégrées avec React.

---
+++ argent-bank/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+// src/pages/Login.jsx
+
+import React from "react"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import LoginForm from "../components/LoginForm"
+
+function Login() {
+  return (
+    <>
+      <Header />
+      <LoginForm />
+      <Footer />
+    </>
+  )
+}
+
+export default Login
